e34e7bee3ce8c21f1d6dcabfee34e2272168e28b
Eric-Arellano <ericarellano@me.com>
2016-09-19 19:39:24 -0500

    pseudocode for abstracted ComparisonHelper class

--- src/features/ComparisonHelper.java
+++ src/features/ComparisonHelper.java
@@ -3,4 +3,97 @@ package features;
 
 public class ComparisonHelper {
 
+	private String comparisonType;
+	private double usageAmount;
+	private double avgAmount;
+	private double minAmount;
+	private double maxAmount;
+
+	// ================================================================================
+	// Constructor and deconstructor
+	// ================================================================================
+
+
+	// ================================================================================
+	// Return comparisons
+	// ================================================================================
+
+	public String compareAvg() {
+
+	}
+
+	public String compareHistorical() {
+
+	}
+
+	private String returnBaseComparison() {
+
+	}
+
+	private String appendGoodJob() {
+
+	}
+
+	private String appendHowMuchToAvg() {
+
+	}
+
+	private String appendHowMuchToMin() {
+
+	}
+
+
+	// ================================================================================
+	// Comparison tests
+	// ================================================================================
+
+	private boolean isGreaterAvg(double usage) {
+		return usage > avgAmount;
+	}
+
+	private boolean isGreaterMax(double usage) {
+		return usage > maxAmount;
+	}
+
+	private boolean isLessMin(double usage) {
+		return usage < minAmount;
+	}
+
+
+	// ================================================================================
+	// Determine comparison type/values
+	// ================================================================================
+
+	private String determineComparisonType() {
+
+	}
+
+	private double determineComparisonAmount() {
+
+	}
+
+	private String determineMoreOrLess() {
+
+	}
+
+
+	// ================================================================================
+	// Calculate differences
+	// ================================================================================
+
+	private double calcAbsoluteDiff() {
+
+	}
+
+	private double calcPercentDiff() {
+
+	}
+
+
+	// ================================================================================
+	// Required input change
+	// ================================================================================
+
+
+
 }
