d0f6a7c2faba81b4b501610c9f219b56884b6fd5
Eric-Arellano <ericarellano@me.com>
2016-12-12 23:40:42 -0700

    update old driver class with master's refactorings

--- src/WaterASUDormOld.java
+++ src/WaterASUDormOld.java
@@ -14,32 +14,33 @@ public class WaterASUDormOld {
 		// Programmer creates several ResourceUsage objects
 		// ================================================================================
 
-		// Shower object
-		// rate = 2.5 gpm, avg = 10.6 min
+		// Shower
+		// rate = 2.5 gpm, avg = 10.6 min, hist = 8.3 min, 9.2 min, 12.4 min, 18.1 min
 		ResourceUsage shower = new UsageTimeDuration("shower",
 				2.5,
-				"gallons",
 				UsageTimeDuration.TimeType.MINUTES,
+				"gallons",
 				"src/tip-files/showerTips.txt",
 				10.6,
 				8.3, 9.2, 12.4, 18.1);
 
-		// Sink object
-		// rate = 1.5 gpm, avg = 0.35 minutes
+		// Sink
+		// rate = 1.5 gpm, avg = 0.35 minutes, hist = 0.2 min, 0.6 min, 0.5 min, 0.9 min
 		ResourceUsage sink = new UsageTimeDuration("sink",
 				1.5,
-				"gallons",
 				UsageTimeDuration.TimeType.SECONDS,
+				"gallons",
 				"src/tip-files/sinkTips.txt",
 				0.35,
 				0.2, 0.6, 0.5, 0.9);
 
-		// Washing Machine object
-		// rate = 25 gal per wash, avg = 4 washes per month
+		// Washing Machine
+		// rate = 25 gal per wash, avg = 4 washes per month, hist = 3 washes, 6 washes, 5 washes, 5
+		// washes
 		ResourceUsage washer = new UsageDiscreteQuantity("washing machine",
 				25,
+				"washes per month",
 				"gallons",
-				"times per month",
 				"src/tip-files/washerTips.txt",
 				4,
 				3, 6, 5, 5);
@@ -92,7 +93,7 @@ public class WaterASUDormOld {
 				System.out.println();
 				chosenUsage.promptInput();
 
-				chosenUsage.calcUsageFromInput();
+				chosenUsage.setUsageFromInput();
 				chosenUsage.displayUsage();
 
 
@@ -108,14 +109,14 @@ public class WaterASUDormOld {
 					System.out.println();
 					switch (menuSelection) {
 						case 1:
-							chosenUsage.compareAverage();
+							chosenUsage.compareToGlobalAverage();
 							System.out.println();
 							break;
 						case 2:
-							chosenUsage.compareHistorical();
+							chosenUsage.compareToHistorical();
 							break;
 						case 3:
-							chosenUsage.displayHistorical();
+							chosenUsage.displayHistoricalUsages();
 							break;
 						case 4:
 							chosenUsage.displayTips();
@@ -123,7 +124,7 @@ public class WaterASUDormOld {
 						case 5:
 							chosenUsage.updateHistoricalBeforeNewInput();
 							chosenUsage.promptInput();
-							chosenUsage.calcUsageFromInput();
+							chosenUsage.setUsageFromInput();
 							chosenUsage.displayUsage();
 							break;
 						case 6:
